c369f685fe1a1596a2b9a0260e488553d5a11bb9
samoilolairina91 <samojlovairina91@gmail.com>
2026-05-07 18:24:55 +0300

    Hello world

---
+++ 1.php
@@ -0,0 +1,25 @@
+<html>
+   <head>
+      <title>Samoilova Irina</title>
+   </head>
+   <body>
+
+      <header style="display: flex; align-items: center; gap: 20px;">
+         <img src="#" 
+              alt="Логотип МосПолитеха" height="50">
+         <h1 style="margin:0">Лабораторная работа: Настройка окружения</h1>
+      </header>
+
+      <main>
+         <?php
+            echo '<h2>Hello, World!</h2>';
+            echo '<p>Динамический контент от PHP: ' . date('Y-m-d H:i:s') . '</p>';
+         ?>
+      </main>
+
+      <footer>
+         <p>задание для самостоятельной работы</p>
+      </footer>
+
+   </body>
+</html>
